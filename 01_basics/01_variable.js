927a6245e383b07aff6a15a19d993627c0c5a7f2
neha-bytedev <neha.saman567@gmail.com>
2025-01-19 08:40:12 +0000

    datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,16 @@
+const accountId = 14457
+let accountEmail= "neha"
+var accountPassword = "12345"
+accountCity="Chandigarh"
+let accountState;
+//accountId=2 not allowed
+
+accountEmail="neha18"
+accountPassword="7890"
+accountCity="Patna"
+
+//console.log(accountId);
+/*
+prefer not to  use because of block  scope issue
+*/
+ console.table([accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_data-types.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all JS as newer version
+
+//alert(3+3)  we are using node js,not browser
+
+console.log(3+
+    3);//readabiltity not high
+console.log("Neha")
+
+/*
+number=> 2 to power 53
+bigint
+string=> ""
+null = standolone value
+undefined
+symbol => unique
+object
+*/
+
+console.log(typeof null);// object
+console.log(typeof undefined);//undefined
+
