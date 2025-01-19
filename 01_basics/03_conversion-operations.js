91e7c06455fe4c0ac866149759e0be1792fc1d27
neha-bytedev <neha.saman567@gmail.com>
2025-01-19 08:56:03 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversion-operations.js
@@ -0,0 +1,21 @@
+let score="33"
+console.log( typeof score);
+console.log(typeof(score));
+
+let valueInNumber= Number (score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+/*
+"33"=>33
+"33abc"=> NaN
+true =>1 / false=> 0
+*/
+let IsLoggedIn=1
+let booleanIsloggedIn= Boolean(IsLoggedIn)
+console.log(booleanIsloggedIn);
+/*
+1 => true / 0 => false
+""=> false / "neha"=> true
+*/
+
