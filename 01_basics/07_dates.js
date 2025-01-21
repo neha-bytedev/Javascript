5ff6e698fb6f058521524debf5f96eee9adfe1cb
neha-bytedev <neha.saman567@gmail.com>
2025-01-21 16:43:24 +0000

    dates

--- 01_basics/07_dates.js
+++ 01_basics/07_dates.js
@@ -9,5 +9,29 @@ let myDate=new Date()
 // console.log(myDate.toLocaleDateString());
 // console.log(myDate.toLocaleTimeString());
 
-let createdDate= new Date(2023,0,23)
-console.log(createDate.toDateString());
+//let createdDate= new Date(2023,0,23)
+// createdDate= new Date(2023,0,23,5,3)
+// console.log(createdDate.toDateString());
+
+// console.log(createdDate.toLocaleString());
+
+let mycreateDate=new Date("2023-01-22")
+console.log(mycreateDate);
+let myTimeStamp=Date.now()
+// console.log(myTimeStamp);
+// console.log(mycreateDate.getTime());
+let newDate=new Date()
+console.log(newDate);
+console.log(newDate.getDate());
+console.log(newDate.getDay());
+console.log(newDate.getMonth()+1);
+
+newDate.toLocaleString('default',{
+    weekday:"narrow"
+})
+
+
+
+
+
+
