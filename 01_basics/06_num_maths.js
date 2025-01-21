b651124d82dc1458073fb176f69420dab5faee37
neha-bytedev <neha.saman567@gmail.com>
2025-01-21 16:07:38 +0000

    date

--- 01_basics/06_num_maths.js
+++ 01_basics/06_num_maths.js
@@ -1,3 +1,33 @@
 const score=400
+console.log(score);
+
 const balance= new Number(100)
-console.log(balance);
+// console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber=23.565667
+// console.log(otherNumber.toPrecision(3));
+const hundreds=100000;
+
+// console.log(hundreds.toLocaleString);
+// console.log(hundreds.toLocaleString('en-IN'));
+//+++++++MATHS ++++++///
+
+console.log(Math);
+
+console.log(Math.abs(-4));
+console.log(Math.round(4.3));
+console.log(Math.ceil(4.3));//upper
+// console.log(Math.floor(4.3));//lower
+console.log(Math.max(4,3,6,7));
+console.log(Math.random());
+console.log((Math.random()*10) +1);
+
+const min=20
+const max=30
+
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
---
+++ 01_basics/07_dates.js
@@ -0,0 +1,13 @@
+//Dates
+
+let myDate=new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleTimeString());
+
+let createdDate= new Date(2023,0,23)
+console.log(createDate.toDateString());
