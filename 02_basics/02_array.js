b632e26af7c5426d9787e94845a9cb17e4ae404b
neha-bytedev <neha.saman567@gmail.com>
2025-01-23 09:53:20 +0000

    #

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -20,5 +20,12 @@ console.log(Array.from("Neha"))
 console.log(Array.from ( {name:"Neha"}))
     // Interesting 
 
+let score1=100
+let score2=200
+let score3=300
+
+console.log(Array.of(score1,score2,score3))
+    
+
 
 
